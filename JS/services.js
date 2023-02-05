aa8080f0b820571ab73f0254475203f5cd2887d5
SaraRamirez16 <124490361+SaraRamirez16@users.noreply.github.com>
2023-02-05 01:45:14 -0800

    Delete JS directory

--- JS/services.js
+++
@@ -1,8 +0,0 @@
-function myFunction(imgs) 
-{
-	var expandImg = document.getElementById("expandedImg");
-	var img = document.getElementById("img");
-	expandImg.src = imgs.src;
-	img.innerHTML = imgs.alt;
-	expandImg.parentElement.style.display = "block";
-}
