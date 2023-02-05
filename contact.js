72c7d865d163124ff0e45f3a0a3cb714f335d126
SaraRamirez16 <124490361+SaraRamirez16@users.noreply.github.com>
2023-02-05 01:20:36 -0800

    Update contact.js

--- contact.js
+++ contact.js
@@ -10,11 +10,10 @@ function myFunction(sent)
 		modal.style.display = "none";
 	}
 	
-		window.onclick = function(event) 
-	{
+	
 		if (event.target == modal) 
 		{
 		modal.style.display = "none";
 		}
-	}
+	
 }
